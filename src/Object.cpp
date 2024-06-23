bdd067ea043388974bc9c92afab496693c948a3d
joshua <joshvigel329@outlook.com>
2024-06-22 19:30:41 -0500

    finished: all code should work now!

--- src/Object.cpp
+++ src/Object.cpp
@@ -6,11 +6,26 @@
 Object::Object(std::string filename) {
     std::ifstream file; 
     file.open(filename);
+    std::cout << file.fail() << " " << filename << std::endl; 
     while (!file.eof()) {
         std::string c;
         std::getline(file, c);
         if (c[0] == 'v') {
+            std::string vert = c.substr(1);
+            std::stringstream ss; ss << vert;  
+            float one, two, three;
+            ss >> one >> two >> three;
+            Points.emplace_back(one * 0.02f, two * 0.02f, three * 0.02f); 
         } else if (c[0] == 'f') {
+            std::string face = c.substr(1);
+            std::stringstream ss; ss << face;
+            int last = -1; 
+            while (!ss.eof()) {
+                int ind; ss >> ind; ind--;
+                if (last != -1)
+                    Edges.emplace_back(last, ind);
+                last = ind;
+            }
         }
     }
     //std::string next = " ";
